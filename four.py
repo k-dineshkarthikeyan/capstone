74289f21c0a91fd3f6fcead01049942faaed9cd2
K Dinesh Karthikeyan <kdineshkarthikeyan@gmail.com>
2024-11-20 15:52:37 +0530

    did some necessary changes

--- four.py
+++ four.py
@@ -3,12 +3,20 @@ import pandas as pd
 from datasets import load_dataset
 from huggingface_hub import login
 from torch.utils.data import DataLoader
-from torchaudio.functional import resample
-import torchaudio
+import os
 
+# os.environ["HF_HOME"] = "/mnt/e/huggingface/"
+# os.environ["HF_DATASETS_DOWNLOADED_DATASETS_PATH"] = "/mnt/e/huggingface/"
 login(token="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")
 
-dataset = load_dataset("mozilla-foundation/common_voice_17_0", "en", num_proc=20)
+
+dataset = load_dataset(
+    "mozilla-foundation/common_voice_17_0",
+    "en",
+    num_proc=20,
+    # cache_dir="/mnt/e/huggingface/",
+    trust_remote_code=True,
+)
 print("downloaded dataset")
 
 
@@ -17,16 +25,19 @@ async def write_csv(split):
     df = pd.DataFrame(data={"speaker_id": [], "audio_path": [], "tensor_shape": []})
 
     for batch in dl:
-        audio = batch["audio"]["array"]
-        audio_sample_rate = torchaudio.info(batch["path"][0]).sample_rate
-        audio = resample(audio, audio_sample_rate, 16000)
-        shape = audio.shape
-        df.loc[len(df)] = [
-            batch["client_id"][0],
-            batch["path"][0],
-            # batch["audio"]["array"].shape,
-            shape,
-        ]
+        # df.loc[len(df)] = [
+        #     batch["client_id"][0],
+        #     batch["path"][0],
+        #     batch["audio"]["array"].shape,
+        # ]
+        new_df = pd.DataFrame(
+            data={
+                "speaker_id": [batch["client_id"][0]],
+                "audio_path": [batch["path"][0]],
+                "tensor_shape": [batch["audio"]["array"].shape],
+            }
+        )
+        df = pd.concat([df, new_df], ignore_index=True)
 
     df.to_csv(f"{split}.csv", index=False)
 
