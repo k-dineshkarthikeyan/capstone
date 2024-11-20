58bdf3851237845706d1a752be187f9258ada9b1
K Dinesh Karthikeyan <kdineshkarthikeyan@gmail.com>
2024-11-20 11:09:38 +0530

    model architecture

---
+++ model.py
@@ -0,0 +1,36 @@
+import torch
+from torch import nn
+import torchaudio
+
+
+class Model(nn.Module):
+    def __init__(self) -> None:
+        super().__init__()
+        bundle = torchaudio.pipelines.WAV2VEC2_XLSR53
+        self.model = bundle.get_model()
+        self.bundle_sample_rate = bundle.sample_rate
+        self.w = nn.Parameter(torch.tensor([1]))
+
+    def first_half(self, support_tensors, query_tensors, labels):
+        avgpool = nn.AdaptiveMaxPool1d((1))
+        out = avgpool(support_tensors)
+        support = out.view((out.size(0), -1))
+
+        out = avgpool(query_tensors)
+        query = out.view((out.size(0), -1))
+
+        n_way = len(torch.unique(labels))
+        proto = torch.cat([
+            support[torch.nonzero(labels == label)].mean(0) for label in range(n_way)
+        ])
+
+        dists = torch.cdist(query, proto)
+        return dists
+
+    def forward(self, support_samples, support_labels, query_samples):
+        support = torch.cat([self.model(sample) for sample in support_samples])
+        query = torch.cat([self.model(sample) for sample in query_samples])
+
+        d = self.first_half(support, query, support_labels)
+        result = self.w * d
+        return result
